5070eac8d0cc3df3ff6c9b33b001b6cdf365d953
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-25 12:40:07 -0700

    Add card styling to booking examples.

--- src/components/BookingA.js
+++ src/components/BookingA.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react'
-// import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+import Card from 'react-bootstrap/Card'
 
 class BookingA extends Component {
     render() {
         return (
-            <div>
-                <h1>Booking Example App A - Acuity - Free Account</h1>
-                {/* <iframe title="acuity" src="https://app.acuityscheduling.com/schedule.php?owner=18492337" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script> */}
-                
-                {/* <h3> Acuity booking button example:</h3> */}
-                <a href="https://app.acuityscheduling.com/schedule.php?owner=18492337" target="_blank" rel="noopener noreferrer" class="acuity-embed-button" style={{background: "#5fb8b9", color: "#fff", padding: "8px 12px", border: "0px", "-webkit-box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "-moz-box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "border-radius": "4px", "text-decoration": "none", display: "inline-block"}}>Schedule Appointment</a><link rel="stylesheet" href="https://embed.acuityscheduling.com/embed/button/18492337.css" id="acuity-button-styles" /><script src="https://embed.acuityscheduling.com/embed/button/18492337.js" async></script>
+            <Card bg="info" text="white" style={{ margin: `2em`}}>
+                <Card.Header as="h3">Booking Acuity - Free Account</Card.Header>
+                <Card.Body>
+                    <Card.Title>Booking button, embed did not work.</Card.Title>
+                    <Card.Text>
+                        <a href="https://app.acuityscheduling.com/schedule.php?owner=18492337" target="_blank" rel="noopener noreferrer" class="acuity-embed-button" style={{ background: "#5fb8b9", color: "#fff", padding: "8px 12px", border: "0px", "-webkit-box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "-moz-box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "border-radius": "4px", "text-decoration": "none", display: "inline-block" }}>Schedule Appointment</a><link rel="stylesheet" href="https://embed.acuityscheduling.com/embed/button/18492337.css" id="acuity-button-styles" /><script src="https://embed.acuityscheduling.com/embed/button/18492337.js" async></script>
 
-                {/* <h3> React Bootstrap embed</h3>
-                <div style={{ width: "100%", height: "800" }}>
-                    <ResponsiveEmbed aspectRatio="16by9">
-                        <embed type="image/svg+xml" src="https://app.acuityscheduling.com/schedule.php?owner=18492337" />
-                    </ResponsiveEmbed>
-                </div> */}
-            </div>
+                    </Card.Text>
+                </Card.Body>
+            </Card>  
         )
     }
 }
--- src/components/BookingB.js
+++ src/components/BookingB.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+import Card from 'react-bootstrap/Card'
 
 class BookingB extends Component {
     render() {
         return (
-            <div>
-                <h1>Booking Example App B</h1>
-            </div>
+            <Card bg="secondary" text="white" style={{ margin: `2em` }}>
+                <Card.Header as="h3">Booking Example Time Tap - Professional</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        <div style={{ width: "100%", height: "800" }}>
+                            <ResponsiveEmbed aspectRatio="16by9">
+                                <embed type="image/svg+xml" src='https://www.timetap.com/emb/309510' />
+                            </ResponsiveEmbed>
+                        </div>
+                        
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         )
     }
 }
