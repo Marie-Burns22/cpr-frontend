90efa049c54ce0dfd7af4e5c14903929080273a9
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-26 15:42:53 -0700

    Add state and componentDidMount with fetch request to CourseContainer. Add Course link to Navigation.

--- src/courseComponents/CourseContainer.js
+++ src/courseComponents/CourseContainer.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react'
 import Course from './Course'
 
 class CourseContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            courses: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://dcpr.herokuapp.com/api/v1/courses', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        courses: result.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
         return (
             <div>
--- src/layoutComponents/Navigation.js
+++ src/layoutComponents/Navigation.js
@@ -11,6 +11,7 @@ export default function Navigation () {
             <Navbar.Brand href="/">Debbie's CPR</Navbar.Brand>
             <Nav className="mr-auto">
                     <Nav.Link href="/"><Button style={{ background: "#8b0000", border: "none"}}>Home</Button></Nav.Link>
+                    <Nav.Link href="/courses"><Button style={{ background: "#8b0000", border: "none"}}>Classes</Button></Nav.Link>
                     <Nav.Link href="/contact"><Button style={{ background: "#8b0000", border: "none" }}>Contact</Button></Nav.Link>
                     <Nav.Link href="/teachers"><Button style={{ background: "#8b0000", border: "none" }}>Teachers</Button></Nav.Link>
                     <Nav.Link href="https://app.acuityscheduling.com/schedule.php?owner=18492337"><a href="https://app.acuityscheduling.com/schedule.php?owner=18492337" target="_blank" rel="noopener noreferrer" class="acuity-embed-button" style={{ background: "#8b0000", color: "#fff", padding: "8px 12px", border: "0px", "-webkit-box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "-moz-box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "box-shadow": "0 -2px 0 rgba(0,0,0,0.15) inset", "border-radius": "4px", "text-decoration": "none", display: "inline-block" }}>Schedule CPR Class</a><link rel="stylesheet" href="https://embed.acuityscheduling.com/embed/button/18492337.css" id="acuity-button-styles" /><script src="https://embed.acuityscheduling.com/embed/button/18492337.js" async></script></Nav.Link>
