41c39e69a5832c6997a91e2e4f40a33456c3c552
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-28 15:55:46 -0700

    Add api call to teacherContainer and render teacher information on teacher components.

--- src/teacherComponents/Teacher.js
+++ src/teacherComponents/Teacher.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Card from 'react-bootstrap/Card'
 
 
-class Teacher extends Component {
-    render() {
-        return(
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                    <Card.Title>{this.props.name}</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-        )
-
-    }
+export default function(props){
+    return(
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+                <Card.Title>{props.teacher.name}</Card.Title>
+                <Card.Text>
+                    {props.teacher.description}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
 }
-
-export default Teacher;
--- src/teacherComponents/TeacherContainer.js
+++ src/teacherComponents/TeacherContainer.js
@@ -2,18 +2,101 @@ import React, { Component } from 'react'
 import Teacher from './Teacher'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner'
 
 class TeacherContainer extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            teachers: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://dcpr.herokuapp.com/api/v1/teachers', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        teachers: result.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+    
+    
     render() {
-        return (
-            <Container>
-            <CardDeck >
-                <Teacher name="Debbie" />
-                <Teacher name="Richard" />
-                <Teacher name="Bridget" />
-            </CardDeck>
-            </Container>
-        )
+        const { error, isLoaded, teachers } = this.state;
+        const teacherCards = teachers.map(t => (
+            <Teacher key={t.id} teacher={t.attributes} id={t.id} />
+        ))
+
+        if (error) {
+            return (
+                <Alert variant="warning">
+                    Error: {error.message}
+                </Alert>
+            )
+        } else if (!isLoaded) {
+            return (
+                <Alert variant="danger">
+                    <ButtonToolbar>
+                        <Button variant="danger" disabled>
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            <span className="sr-only">Loading...</span>
+                        </Button>
+                        <Button variant="danger" disabled>
+                            <Spinner
+                                as="span"
+                                animation="grow"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            Loading...
+                            </Button>
+                    </ButtonToolbar>
+                </Alert>
+            )
+        } else if (!teachers || teachers === undefined || teachers.length === 0) {
+            return (
+                <Alert variant="danger">
+                    Please contact us using <Alert.Link href="/contact">the Contact Form</Alert.Link>for more information about our teachers.
+                </Alert>
+            )
+        } else {
+            return (
+                <Container>
+                    <CardDeck>
+                        {teacherCards}
+                    </CardDeck>
+                </Container>
+            )
+        }
     }
 }
 
