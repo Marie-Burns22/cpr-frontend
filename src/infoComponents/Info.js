380e64cc28486c40e65a08c4bb3c71bcbaebf649
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-28 15:43:52 -0700

    Add api request for info in infoContainer and render with Info components.

--- src/infoComponents/Info.js
+++ src/infoComponents/Info.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Card from 'react-bootstrap/Card'
 
-class Info extends Component {
-    render() {
-        return (
-            <Card bg="#eeeeee" style={{ margin: "2em 0em 2em 0em" }}> 
-                <Card.Header as="h3" style={{ background: "#bf0000", color: "white"}}>{this.props.title}</Card.Header>
-                <Card.Body style={{ background: "#eeeeee"}}>
-                    {/* <Card.Title>Info Card Title</Card.Title> */}
-                    <Card.Text>
-                        {this.props.content}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        )
-    }
+export default function(props) {
+    return (
+        <Card bg="#eeeeee" style={{ margin: "2em 0em 2em 0em" }}> 
+            <Card.Header as="h3" style={{ background: "#bf0000", color: "white"}}>{props.info.title}</Card.Header>
+            <Card.Body style={{ background: "#eeeeee"}}>
+                <Card.Text>
+                    {props.info.content}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
 }
-export default Info;
--- src/infoComponents/InfoContainer.js
+++ src/infoComponents/InfoContainer.js
@@ -2,16 +2,99 @@ import React, { Component } from 'react'
 import Info from './Info'
 import GoogleMap from "./GoogleMap"
 import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner'
 
 class InfoContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            infos: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://dcpr.herokuapp.com/api/v1/infos', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        infos: result.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        return (
-            <Container>
-                <Info title="Cards" content="Cards will be emailed after the course" />
-                <Info title="Arrival and Checkin" content="Please arrive 15 min before the start of class to sign in" />
-                <GoogleMap />
-            </Container>
-        )
+        const { error, isLoaded, infos } = this.state;
+        const infoCards = infos.map(i => (
+            <Info key={i.id} info={i.attributes} id={i.id} />
+        ))
+
+        if (error) {
+            return (
+                <Alert variant="warning">
+                    Error: {error.message}
+                </Alert>
+            )
+        } else if (!isLoaded) {
+            return (
+                <Alert variant="danger">
+                    <ButtonToolbar>
+                        <Button variant="danger" disabled>
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            <span className="sr-only">Loading...</span>
+                        </Button>
+                        <Button variant="danger" disabled>
+                            <Spinner
+                                as="span"
+                                animation="grow"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            Loading...
+                            </Button>
+                    </ButtonToolbar>
+                </Alert>
+            )
+        } else if (!infos || infos === undefined || infos.length === 0) {
+            return (
+                <Alert variant="danger">
+                    Please contact us using <Alert.Link href="/contact">the Contact Form</Alert.Link>for more information regarding available courses.
+                </Alert>
+            )
+        } else {
+            return (
+                <Container>
+                    {infoCards} 
+                    <GoogleMap />
+                </Container>
+            )
+        }
     }
 }
 
