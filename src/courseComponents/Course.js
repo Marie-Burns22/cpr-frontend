830220eb48f12bb027ce977330f446908267404e
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-28 16:08:56 -0700

    Add conditional to course cards to render button to contact form if no fee is provided by api.

--- src/courseComponents/Course.js
+++ src/courseComponents/Course.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
 
-class Course extends Component {
-    render() {
-        return (
-            <Container>
-                <Card style={{ margin: "2em 0em 2em 0em" }}>
-                    <Card.Header as="h3" style={{ background: "#bf0000", color: "#eeeeee"}}>{this.props.course.title}</Card.Header>
-                    <Card.Body style={{ background: "#eeeeee" }}>
-                        <Card.Title>Course Fee: ${this.props.course.fee}</Card.Title>
-                        <Card.Text text="black">
-                            {this.props.course.description}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Container>
-        )
-    }
+
+export default function(props) {
+    const fee = props.course.fee ? `Course Fee: $${props.course.fee}` : <Link to="/contact"><Button style={{ background: "#8b0000", border: "none" }}>Contact for fee information</Button></Link>
+    return (
+        <Container>
+            <Card style={{ margin: "2em 0em 2em 0em" }}>
+                <Card.Header as="h3" style={{ background: "#bf0000", color: "#eeeeee"}}>{props.course.title}</Card.Header>
+                <Card.Body style={{ background: "#eeeeee" }}>
+                    <Card.Title> {fee} </Card.Title>
+                    <Card.Text text="black">
+                        {props.course.description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
 }
-
-export default Course
