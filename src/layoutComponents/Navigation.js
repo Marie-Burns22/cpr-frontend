f0beb7c821723c8c4997b0068d6f59f4bf85fa22
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-28 16:58:29 -0700

    Fix navbar styling.

--- src/layoutComponents/Navigation.js
+++ src/layoutComponents/Navigation.js
@@ -6,16 +6,19 @@ import Button from 'react-bootstrap/Button'
 
 export default function Navigation () {
     return (
-        <Navbar variant="dark" sticky="top" style={{ background: "#ffffff" }}>
-            <Container>
-                <Nav className="mr-auto" >
-                    <Nav.Link href="/"><Button style={{ background: "#8b0000", border: "none"}}>Home</Button></Nav.Link>
-                    <Nav.Link href="/courses"><Button style={{ background: "#8b0000", border: "none"}}>Classes</Button></Nav.Link>
-                    <Nav.Link href="/contact"><Button style={{ background: "#8b0000", border: "none" }}>Contact</Button></Nav.Link>
-                    <Nav.Link href="/teachers"><Button style={{ background: "#8b0000", border: "none" }}>Teachers</Button></Nav.Link>
-                    <Nav.Link href="https://app.acuityscheduling.com/schedule.php?owner=18492337" target="_blank" rel="noopener noreferrer"><Button style={{ background: "#8b0000", border: "none" }}>Book a CPR Class</Button></Nav.Link>
-                </Nav>
-            </Container>
+        // <Navbar variant="dark" sticky="top" style={{ background: "#ffffff" }} expand="lg">
+        <Navbar sticky="top" style={{ background: "#ffffff" }} className="justify-content-between container" expand="md">
+            <Navbar.Brand href="/">Debbie's CPR</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto" className="justify-content-center container">
+                        <Nav.Link href="/"><Button style={{ background: "#8b0000", border: "none"}}>About</Button></Nav.Link>
+                        <Nav.Link href="/courses"><Button style={{ background: "#8b0000", border: "none"}}>Classes</Button></Nav.Link>
+                        <Nav.Link href="/contact"><Button style={{ background: "#8b0000", border: "none" }}>Contact</Button></Nav.Link>
+                        <Nav.Link href="/teachers"><Button style={{ background: "#8b0000", border: "none" }}>Teachers</Button></Nav.Link>
+                        <Nav.Link href="https://app.acuityscheduling.com/schedule.php?owner=18492337" target="_blank" rel="noopener noreferrer"><Button style={{ background: "#8b0000", border: "none" }}>Book a CPR Class</Button></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>  
         </Navbar>
     )
 }
