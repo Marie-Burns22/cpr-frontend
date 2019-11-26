43dc3c5ed5bf819e0997c80752d5d1ed1e661b26
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-11-25 22:06:33 -0800

    Reorder navigation buttons.

--- src/layoutComponents/Navigation.js
+++ src/layoutComponents/Navigation.js
@@ -11,10 +11,10 @@ export default function Navigation () {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto justify-content-center container">
                         <Nav.Link href="/"><Button style={{ background: "#8b0000", border: "none"}}>Info</Button></Nav.Link>
-                        <Nav.Link href="/contact"><Button style={{ background: "#8b0000", border: "none" }}>Contact</Button></Nav.Link>
+                        <Nav.Link href="/"><Button style={{ background: "#8b0000", border: "none"}}>Book CPR BLS Course</Button></Nav.Link>
                         <Nav.Link href="/teachers"><Button style={{ background: "#8b0000", border: "none" }}>Instructors</Button></Nav.Link>
                         <Nav.Link href="/courses"><Button style={{ background: "#8b0000", border: "none"}}>Private Group Courses</Button></Nav.Link>
-                        <Nav.Link href="/"><Button style={{ background: "#8b0000", border: "none"}}>Book CPR BLS Course</Button></Nav.Link>
+                        <Nav.Link href="/contact"><Button style={{ background: "#8b0000", border: "none" }}>Contact</Button></Nav.Link>
                         <Nav.Link href="https://ecards.heart.org/student/myecards?pid=ahaecard.employerStudentSearch"><Button style={{ background: "#8b0000", border: "none"}}>Replacement Cards</Button></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>  
