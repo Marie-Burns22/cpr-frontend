de50c05d055d557b8cf0b030cb36e7181e1b8b54
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2020-01-14 09:46:59 -0800

    Add direct link to scheduling page.

--- src/infoComponents/Scheduling.js
+++ src/infoComponents/Scheduling.js
@@ -13,6 +13,7 @@ export default function Scheduling() {
                     <Card.Title>Available dates are shown in bold on the calendar below.</Card.Title> 
                     <Card.Title>Click on an available date to book.</Card.Title>
                     <Card.Title>Plan to arrive 15 minutes early to register for your CPR card.</Card.Title>
+                    <Card.Title>If the calendar does not appear below, click <a href="https://app.acuityscheduling.com/schedule.php?owner=18637332">here</a> to visit the scheduling page.</Card.Title>
                 </Card.Body>
             <Card.Body style={{ background: "#eeeeee" }}>
             <iframe title="scheduling" src="https://app.acuityscheduling.com/schedule.php?owner=18637332" width="100%" height="800" frameBorder="0"></iframe> 
