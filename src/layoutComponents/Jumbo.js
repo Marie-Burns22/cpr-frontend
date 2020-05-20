1a981de7ada433fbfe766b46e88a8ac18e0751af
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2020-05-20 08:59:59 -0700

    Update coronavirus message for opening.

--- src/layoutComponents/Jumbo.js
+++ src/layoutComponents/Jumbo.js
@@ -10,8 +10,10 @@ export default function Jumbo() {
             <Container style={{ marginBottom: "2em" }}>
                 <h1>Debbie's CPR</h1>
                 <h4 style={{ color: "white"}}>
-                    {/* Our family offers decades of professional experience teaching personalized CPR courses for professionals and everyone interested in learning life saving skills. */}
-                    Due to the coronavirus outbreak, all CPR classes have been canceled. Kaiser employees have been given a 120-day extension on CPR card expiration date. All other employees need to ask their employers what policy will be enforced. To get future class dates please check back periodically at this website. We will resume classes once we are advised by health departments to do so.
+                    We are happy to announce that we are able to resume teaching CPR classes in early June. Class dates will be posted and available for signup by the end of the week starting May 18.
+
+                    To keep all students and teachers safe, we will follow CDC guidelines. Please help us to keep everyone healthy by canceling or rescheduling if you are feeling sick or experiencing any symptoms that may indicate Covid-19. All surfaces that students and teachers use will be thoroughly sanitized between each class. Hand sanitizer will be provided for all students. Students are required to bring their own mask and wear it throughout the class. Please contact us using the contact button if you have any further questions or concerns.
+                    {/* Due to the coronavirus outbreak, all CPR classes have been canceled. Kaiser employees have been given a 120-day extension on CPR card expiration date. All other employees need to ask their employers what policy will be enforced. To get future class dates please check back periodically at this website. We will resume classes once we are advised by health departments to do so. */}
                 </h4>
             </Container>
         </Jumbotron>
